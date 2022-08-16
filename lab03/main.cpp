0b72336dd64d05a2c9b7355c70905b3a762cc7f6
lhardt <lhardt@lhardt.hardt>
2022-08-16 09:21:58 -0300

    Almost working

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -17,7 +17,13 @@ public:
     os << word.text << " " << word.n_times << std::endl;
     return os;
   }
-  // friend
+
+  Word(const Word& other){
+    for(int i = 0; i < STR_SZ; ++i) text[i] = other.text[i];
+    len = other.len;
+    n_times = other.n_times;
+
+  }
 };
 
 bool should_ignore(Word & word){
@@ -48,39 +54,93 @@ std::vector<Word> read_file(int & max_len){
     fix_word(next_word);
 
     max_len = max_len > next_word.len ? max_len : next_word.len;
-
     if(should_ignore(next_word))
       continue;
 
     vec.push_back( next_word );
   }
 
+
   return vec;
 }
+
+#define SWP(type, a,b) do{ type tmp; tmp = a; a = b; b = tmp; }while(0)
+
+void remove_repeated(std::vector<Word> & list){
+  for(int i = 1; i < list.size();++i){
+    if( 0 == strcmp(list[i].text, list[i-1].text) ){
+      list[i-1].n_times += list[i].n_times;
+      list.erase(list.begin() + i);
+      --i;
+    }
+  }
+}
+
 
 void radix_sort(std::vector<Word> & list, int last_pos){
   int count[256] = {0};
 
-  for(int i = 0; i < 256; ++i){ count[i] = 0; }
+  std::vector<Word> list_copy(list);
+
+  std::vector<Word> *list_a = &list;
+  std::vector<Word> *list_b = &list_copy;
 
   int n_total = list.size();
-  while(n_total != count[0]){
+  for(int pos = last_pos-1; pos >= 0; --pos){
+    for(int i = 0; i < 256; ++i){ count[i] = 0; }
+    SWP(std::vector<Word>*, list_a, list_b);
+    // counts them all
+    for(int i = 0; i < list_a->size(); ++i){
+      Word this_word = list_a->at(i);
+      if(this_word.len <= pos){
+        count[0]++;
+      } else {
+        count[ this_word.text[pos] ]++;
+      }
+    }
 
-    for(int pos)
+    if(count[0] == list.size())
+      break;
+
+    // aggregates
+    for(int i = 1; i < 256; ++i){
+      count[i] += count[i-1];
+    }
+    // puts them in order
+    for(int i = list.size()-1; i >= 0; --i){
+      Word this_word = list_a->at(i);
+      int new_pos;
+      if(this_word.len <= pos){
+        new_pos = --count[0];
+        // printf("new pos is %d\n", new_pos);
+      } else {
+        new_pos = --count[ this_word.text[pos] ];
+      }
+
+      (*list_b)[new_pos] = list_a->at(i);
+
+      // check if there are repeated words.
+    }
   }
+  // list_b was the copied_into from the last iteration
+  for(int i = 0; i < list.size(); ++i){
+    list[i] = list_b->at(i);
+  }
+
+  remove_repeated(list);
 }
 
 int main(){
-  int last_pos;
-  std::vector<Word> list_of_words = read_file(&last_pos);
+  int last_pos = 0;
+  std::vector<Word> list_of_words = read_file(last_pos);
+
+
+  radix_sort(list_of_words, last_pos);
 
   for(auto word : list_of_words){
     std::cout << word;
   }
-
-  radix_sort(list, last_pos);
-
-  printf("n_words: %d\n", list_of_words.size());
+  printf("\nn_words: %d\n", list_of_words.size());
 
   return 0;
 }
